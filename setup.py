c827e3aeecc029dcb060f35d1ff9aadec7b346bf
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-04-27 17:14:05 +0200

    chore: Bump up the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="nettirely",
-    version="1.1.0",
+    version="2.0.0",
     description="IRC bots for humans.",
     keywords="irc bot ircbot humans",
     author="The 8Banana Team",
